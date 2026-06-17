33c8af17fc7c9f31ab51b6fe37c47e53c0c7c828
小室祥吾 <komuroshougo@komuroshougonoMacBook-Air.local>
2026-06-17 20:54:48 +0900

    テスト修正：ルーティングヘルパーの修正とログイン処理の追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # ログインヘルパーを読み込む（Deviseを使っている場合）
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    # テスト用のユーザー（fixtures/users.yml にあるもの）を取得してログイン
+    @user = users(:one) 
+    sign_in @user
+  end
+
   test "should get top" do
-    # ルートパス（トップページ）へアクセス
     get root_path
     assert_response :success
   end
--- test/controllers/survival_logs_controller_test.rb
+++ test/controllers/survival_logs_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class SurvivalLogsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get survival_logs_index_url
+    get survival_logs_url
     assert_response :success
   end
 
   test "should get create" do
-    get survival_logs_create_url
+    get survival_logs_url
     assert_response :success
   end
 end
